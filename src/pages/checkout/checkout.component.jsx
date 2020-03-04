84974dc9c69e928a0835d12ff5f668c510d52187
Tim Kim <timkim@pop-os.localdomain>
2020-03-04 15:02:20 -0800

    added react-stripe payment package and made new stripe pay button component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from "reselect";
 import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import "./checkout.styles.scss";
 
@@ -35,6 +36,12 @@ const CheckoutPage = ({cartItems, cartTotal}) => (
     <div className="total">
         <span>TOTAL: ${cartTotal}</span>
     </div>
+    <div className="test-warning">
+      *PLease use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/21 (or whatever the next year is) - CVV: 123
+    </div>
+    <StripeCheckoutButton price={cartTotal} />
   </div>
 );
 
